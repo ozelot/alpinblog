05d8d1ed780b1b38dffb992579340a8ddedc9857
Andreas Stehling <as@picopay.de>
2011-02-15 14:09:11 +0100

    Add blogpost and sequence factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,16 @@
 # By using the symbol :user we get Factory Girl to simulate the User model
 Factory.define :user do |user|
   user.name                  "Andreas Stehling"
-  user.email                 "stehling.andreas@gmail.com"
+  user.email                 "hero@gmail.com"
   user.password              "foobar"
   user.password_confirmation "foobar"
 end
 
 Factory.sequence :email do |n|
   "person-#{n}@example.com"
+end
+
+Factory.define :blogpost do |blogpost|
+  blogpost.content "Foo Bar"
+  blogpost.association :user
 end
