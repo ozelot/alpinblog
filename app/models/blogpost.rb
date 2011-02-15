2b224e390f596118977cc4be62b12fc8d33bc850
Andreas Stehling <as@picopay.de>
2011-02-15 14:04:10 +0100

    Add basic blogpost model

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -1,2 +1,15 @@
 class Blogpost < ActiveRecord::Base
+  attr_accessible :title, :subtitle, :content
+
+  belongs_to :user
+
+  default_scope :order => 'blogposts.created_at DESC'
+
+  validates :user_id, :presence => true
+  validates :title, :presence => true,
+                    :length => {:maximum => 50}
+  validates :subtitle, :presence => true,
+                       :length => {:maximum => 100}
+  validates :content, :presence => true,
+                      :length => {:maximum => 2000}
 end
--- spec/models/blogpost_spec.rb
+++ spec/models/blogpost_spec.rb
@@ -1,5 +1,62 @@
 require 'spec_helper'
 
 describe Blogpost do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @user = Factory(:user)
+    @attr = {:title => "The title",
+             :subtitle => "This it the subtitle",
+             :content => "This is the content."}
+  end
+
+  it "should create a new instance" do
+    @user.blogposts.create!(@attr)
+  end
+
+  describe "user associations" do
+
+    before(:each) do
+      @blogpost = @user.blogposts.create!(@attr)
+    end
+    
+    it "should have a user attribute" do
+      @blogpost.should respond_to(:user)
+    end
+
+    it "should belong to the right user" do
+      @blogpost.user_id.should == @user.id
+      @blogpost.user.should == @user
+    end
+  end
+
+  describe "validations" do
+    
+    it "should require a user attribute" do
+      Blogpost.new(@attr).should_not be_valid
+    end
+
+    it "should require a non-empty title" do
+      @user.blogposts.build(:title => "").should_not be_valid
+    end
+
+    it "should require a non-empty subtitle" do
+      @user.blogposts.build(:subtitle => "").should_not be_valid
+    end
+
+    it "should require a non-empty content" do
+      @user.blogposts.build(:content => "").should_not be_valid
+    end
+
+    it "should reject too long title" do
+      @user.blogposts.build(:title => "a"*50).should_not be_valid
+    end
+
+    it "should reject too long subtitle" do
+      @user.blogposts.build(:subtitle => "a"*100).should_not be_valid
+    end
+
+    it "should reject too long content" do
+      @user.blogposts.build(:content => "a"*2000).should_not be_valid
+    end
+  end
 end
