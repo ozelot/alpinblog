501294d85f4f752759c3b8e55a7eb5d5fc056e4b
Andreas Stehling <as@picopay.de>
2011-02-15 14:47:31 +0100

    Make blogpost resource accessible from web

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Alpinblog::Application.routes.draw do
 
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
+  resources :blogposts
 
   match '/signup',  :to => 'users#new'
   match '/signin',  :to => 'sessions#new'
