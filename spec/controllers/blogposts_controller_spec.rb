1299ae5dfbb5577abd4f1e6d326a3e90e8a62246
Andreas Stehling <as@picopay.de>
2011-02-15 21:24:34 +0100

    Add test for blogposts action:create

--- spec/controllers/blogposts_controller_spec.rb
+++ spec/controllers/blogposts_controller_spec.rb
@@ -30,6 +30,38 @@ describe BlogpostsController do
   end
 
   describe "POST 'create'" do
+    
+    before(:each) do
+      @user = Factory(:user)
+      @blogpost = {:title => "The Title",
+                   :subtitle => "The subtitle",
+                   :content => "The content."}
+      test_sign_in(@user)
+    end
+
+    describe "failure" do
+      
+      it "should not create a blogpost with no title" do 
+        lambda do
+          post :create, :blogpost => @blogpost.merge(:title => "")
+         end.should_not change(Blogpost, :count)
+      end
+      
+      it "should render the home page" do
+        post :create, :blogpost => @blogpost.merge(:title => "")
+        response.should render_template('blogposts/new')
+      end   
+    end
+
+    describe "success" do
+      
+      it "should create a new blogpost" do 
+        lambda do
+          post :create, :blogpost => @blogpost
+          response.should redirect_to(root_path)
+        end.should change(Blogpost, :count).by(1)
+      end
+    end
   end
 
   describe "PUT 'update'" do
