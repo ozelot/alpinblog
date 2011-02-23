9229b6bb2eaa982b75d54caa94cb9839543d7bfa
Andreas Stehling <as@picopay.de>
2011-02-23 15:05:19 +0100

    Change logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Return logo image
   def logo
-      image_tag("logo.png", :alt => "Sample App", :class => "round")
+      image_tag("logo.png", :alt => "Alpine Blog")
   end
 
   # Return a title on per page basis
